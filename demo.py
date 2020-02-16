a070931595b970eb1c5b0a845225304ae8e3937e
wangzhen <wangzhen2@163.com>
2020-02-16 16:34:19 +0800

    新版本

---
+++ demo.py
@@ -0,0 +1,20 @@
+import time
+
+print(time.sleep(2))
+
+print("helloword")
+print("HAHA")
+
+print("课哦 ")
+
+print("执行git commit")
+
+
+print("执行git commit")
+
+print("Another git process seems to be running in this repository, e.g.
+an editor opened by 'git commit'. Please make sure all processes
+are terminated then try again. If it still fails, a git process
+may have crashed in this repository earlier:
+remove the file manually to continue.
+")
